68f170e9dc686376ac5623e97fcc5798e92941fd
SrWither <fjachuf@gmail.com>
2024-02-27 21:22:34 -0300

    green to purple

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				background: '#040D12',
-				primary: '#183D3D',
-				secondary: '#5C8374',
-				accent: '#93B1A6'
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#22143d',
+        primary: '#4C2A85',
+        secondary: '#6C4BBD',
+        accent: '#8D77DC'
+      }
+    },
+  },
+  plugins: [],
 }
